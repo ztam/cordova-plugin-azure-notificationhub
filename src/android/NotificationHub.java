ec53a8a99d1b8367b929c4dfd61e1caca39e3885
Craig Presti <craig@seventhcap.com>
2015-11-19 15:02:40 -0800

    Cherry picked fixes from @mvanhalen so notifications on Android are displayed in the tray
    Fixes bug where notifications on Android aren't displayed when the app
    isn't in the foreground

--- src/android/NotificationHub.java
+++ src/android/NotificationHub.java
@@ -3,18 +3,22 @@ package msopentech.azure;
 import java.util.Set;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.v4.app.NotificationCompat;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
-import com.microsoft.windowsazure.messaging.GcmNativeRegistration;
 import com.microsoft.windowsazure.messaging.Registration;
 
 /**
@@ -117,6 +121,19 @@ public class NotificationHub extends CordovaPlugin {
         @Override
         public void onReceive(Context context, Intent intent) {
             
+			//always display system notification
+			String contentTitle = "CordovaApp";
+			String contentMessage = intent.getExtras().get("message").toString();
+
+			try {
+				contentTitle = context.getString(context.getApplicationInfo().labelRes);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+
+            displayNotification(context, getDrawableIcon(context), contentTitle, contentMessage);
+
             if (NotificationHub.getCallbackContext() == null){
                 return;
             }                                    
@@ -134,7 +151,38 @@ public class NotificationHub extends CordovaPlugin {
                 e.printStackTrace();
             }
         }
-        
+		
+		private void displayNotification(Context context, Integer icon, String contentTitle, String contentMessage) {
+
+            PendingIntent contentIntent = PendingIntent.getActivity(context.getApplicationContext(), 0,
+                    new Intent(context.getApplicationContext(), context.getApplicationContext().getClass()), 0);
+
+
+            NotificationCompat.Builder mBuilder =
+					new NotificationCompat.Builder(context)
+					.setSmallIcon(icon)
+					.setContentTitle(contentTitle)
+					.setContentText(contentMessage);
+		
+			mBuilder.setContentIntent(contentIntent);
+			mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+			mBuilder.setAutoCancel(true);
+		
+			NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+			mNotificationManager.notify(1, mBuilder.build());
+		}
+
+        private int getDrawableIcon (Context ctx) {
+            Context context = ctx.getApplicationContext();
+            String pkgName = context.getPackageName();
+
+            int resId;
+            resId = context.getResources().getIdentifier("icon", "drawable", pkgName);
+
+            return resId;
+        }
+
+
     }
     
     /**
